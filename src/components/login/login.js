34e2b654a67e8677644b2fa7c5bc302f5126646a
Joshua Dale <jdale9930@gmail.com>
2021-04-12 12:09:24 -0500

    added user in redux

---
+++ src/components/login/login.js
@@ -0,0 +1,8 @@
+import {react, useState} from "react"
+
+const Login = () => {
+    const[username, setUsername] = useState("")
+    const[password, setPassword] = useState("")
+}
+
+export default Login
